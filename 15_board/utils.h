4f89811d278935d839bef87a8cf8fd54e3bf0ff7
Gleb <barabashka.zzz@gmail.com>
2017-12-11 05:45:43 +0300

    hate cpp

--- 15_board/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-int mod(int a, unsigned n);  // in c++  -1 % 4 = -1 or 3 but i want = 3 always
-unsigned manh_distance_in_matrix(int a, int b,
-                                 int n);  // a b - coords, n - one side size
-
-#endif
